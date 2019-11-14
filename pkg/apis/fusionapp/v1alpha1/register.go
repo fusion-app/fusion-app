e04083a2f2f2c661751304dd0c0f32b1af165ac3
Solomonwisdom <whg19962015@163.com>
2019-11-14 11:10:33 +0800

    add interface

--- pkg/apis/fusionapp/v1alpha1/register.go
+++ pkg/apis/fusionapp/v1alpha1/register.go
@@ -6,13 +6,14 @@
 package v1alpha1
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/scheme"
 )
 
 var (
+	// SchemeGroupVersion is group version used to register these objects
+	SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: GroupVersion}
+
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
 
@@ -22,22 +23,8 @@ var (
 const (
 	// GroupName is the group name use in this package.
 	GroupName = "fusionapp.io"
-	// Kind is the kind name.
-	Kind = "Resource"
 	// GroupVersion is the version.
 	GroupVersion = "v1alpha1"
-	// Plural is the Plural for Workspace.
-	Plural = "resources"
-	// Singular is the singular for Workspace.
-	Singular = "resource"
-)
-
-var (
-	// SchemeGroupVersion is group version used to register these objects
-	SchemeGroupVersion = schema.GroupVersion{Group: GroupName, Version: GroupVersion}
-
-	// SchemeGroupVersionKind is the GroupVersionKind of the resource.
-	SchemeGroupVersionKind = SchemeGroupVersion.WithKind(Kind)
 )
 
 // Resource takes an unqualified resource and returns a Group-qualified GroupResource.
@@ -45,12 +32,3 @@ func GroupResource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
 }
 
-// addKnownTypes adds the set of types defined in this package to the supplied scheme.
-func addKnownTypes(scheme *runtime.Scheme) error {
-	scheme.AddKnownTypes(SchemeGroupVersion,
-		&Resource{},
-		&ResourceList{},
-	)
-	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
-	return nil
-}
